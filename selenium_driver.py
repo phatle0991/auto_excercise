a832455ca91392895e1286edb33c242a86a8b72b
Phat Le <phat@geocomply.com>
2021-08-18 18:35:29 +0700

    Initial commit

---
+++ selenium_driver.py
@@ -0,0 +1,39 @@
+"""
+Driver Util using to:
+- initialize Web Driver. We support Chrome, Firefox. Manage driver version by webdriver_manager
+- get Web Driver
+- Web Driver actions: Close Driver
+@author by phat.le on Aug 18, 2021
+"""
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
+class Driver:
+    def __init__(self):
+        self._driver = None
+
+    def create_driver(self, browser_name="chrome"):
+        if browser_name.lower() == "chrome":
+            return self.init_chrome_browser()
+        elif browser_name.lower() == "firefox":
+            return self.init_firefox_browser()
+        else:
+            raise Exception("Browser not supported. We are supported for: Chrome, Firefox")
+
+    def get_driver(self):
+        return self._driver
+
+    def quit_driver(self):
+        self._driver.quit()
+
+    def init_chrome_browser(self):
+        self._driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+        self._driver.implicitly_wait(5)
+        return self._driver
+
+    def init_firefox_browser(self):
+        self._driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+
+        self._driver.implicitly_wait(5)
+        return self._driver
---
+++ test.py
@@ -0,0 +1,6 @@
+from selenium_driver import Driver
+
+Driver = Driver()
+
+driver = Driver.create_driver()
+driver.get("https://google.com")
